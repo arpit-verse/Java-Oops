c5abd1b32d1da3727ce3e861571b123fd8d6bc32
ARPIT SINGH <arpit.singh.cs28@iilm.edu>
2025-08-18 23:49:28 +0530

    Update first.java

--- first.java
+++ first.java
@@ -2,8 +2,8 @@ public class First {
     // print your name 01
     // print your address 02
     public static void main(String[] args){
-        System.out.println("Aditi Sharma ");
-        System.out.println("CET girls hostel- 101");
+        System.out.println("arpit ");
+        System.out.println("noida");
 
 
     }
